2eea68eb5970748ed94afe1a6189cdc7328e28d9
yamanahmet <ymna32@gmail.com>
2021-11-24 01:05:20 +0300

    Seed function added

--- Day4/Day 4.1 - Heads or Tails.py
+++ Day4/Day 4.1 - Heads or Tails.py
@@ -1,10 +1,16 @@
-#Write your code below this line 👇
-#Hint: Remember to import the random module first. 🎲
-
 import random
+#Remember to use the random module
+#Hint: Remember to import the random module first. 🎲
+	 
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+ # 🚨 Don't change the code above 👆 It's only for testing your code.
+	 
+#Write your code below this line 👇
 
 random_int = random.randint(0, 1)
-if random_int == 0:
-  print("Heads")
+if random_int == 1:
+    print("Heads")
 else:
-  print("Tails")
+    print("Tails")
