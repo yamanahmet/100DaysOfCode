7b0226f659f9731b41bf4bb10a6e6f2bf2dafa17
yamanahmet <64093249+yamanahmet@users.noreply.github.com>
2021-11-24 16:00:27 +0300

    Update Day 4.3 - Treasure Map.py
    Updated using array

--- Day4/Day 4.3 - Treasure Map.py
+++ Day4/Day 4.3 - Treasure Map.py
@@ -9,11 +9,10 @@ position = input("Where do you want to put the treasure? ")
 
 #Write your code below this row 👇
 
-int_position = int(position)
-x = int(int_position / 10)
-y = int_position % 10
+horizonal = int(position[0])
+vertical = int(position[1])
 
-map[x-1][y-1] = "X"
+map[vertical - 1][horizonal - 1] = "X"
 
 #Write your code above this row 👆
 
