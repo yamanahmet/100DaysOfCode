bf7be53da100bd87447c70bf5c578e0ea4aba25a
yamanahmet <64093249+yamanahmet@users.noreply.github.com>
2021-11-24 17:26:34 +0300

    Update Day 4 Project - Rock-Paper-Scissors.py
    Got rid of unnecessary checks

--- Day4/Day 4 Project - Rock-Paper-Scissors.py
+++ Day4/Day 4 Project - Rock-Paper-Scissors.py
@@ -28,45 +28,26 @@ scissors = '''
 
 #Write your code below this line 👇
 
+game_images = [rock, paper, scissors]
+
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-if user_choice == 0:
-  print(rock)
-elif user_choice == 1:
-  print(paper)
+if user_choice >= 3 or user_choice < 0:
+  print("You typed an invalid number, you lose!")
 else:
-  print(scissors)
+  print(game_images[user_choice])
+  
+  computer_choice = random.randint(0, 2)
+  print("Computer chose:")
+  print(game_images[computer_choice])
 
-computer_choice = random.randint(0, 2)
-print(f"Computer chose: {computer_choice}")
-
-if computer_choice == 0:
-  print(rock)
-elif computer_choice == 1:
-  print(paper)
-else:
-  print(scissors)
-
-if user_choice == 0:
-  if computer_choice == 0:
-    print("It's a draw")
-  elif computer_choice == 1:
-    print("You lose")
-  else:
+  if user_choice == 0 and computer_choice == 2:
     print("You win!")
-
-if user_choice == 1:
-  if computer_choice == 0:
-    print("You win!")
-  elif computer_choice == 1:
-    print("It's a draw")
-  else:
+  elif user_choice == 2 and computer_choice == 0:
     print("You lose")
-
-if user_choice == 2:
-  if computer_choice == 0:
-    print("You lose")
-  elif computer_choice == 1:
+  elif user_choice > computer_choice:
     print("You win!")
+  elif user_choice < computer_choice:
+    print("You lose")
   else:
     print("It's a draw")
