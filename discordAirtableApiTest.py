bf1f6aa5cf591042fd2b131a0ee63287fb67cc2b
Ahmet YAMAN <64093249+yamanahmet@users.noreply.github.com>
2022-02-22 21:47:54 +0300

    Update discordAirtableApiTest.py

--- discordAirtableApiTest.py
+++ discordAirtableApiTest.py
@@ -1,7 +1,7 @@
 from pyairtable import Api, Base, Table
 
-api = Api('keyTRu7XDdf9Ky5Q0')  //apikey
-api.all('appZg83GywzAAXvlu', 'Contacts') //base_id, table_name
+api = Api('keyTRu7XDdf9Ky5Q0')  #apikey
+api.all('appZg83GywzAAXvlu', 'Contacts') #base_id, table_name
 
 base = Base('keyTRu7XDdf9Ky5Q0', 'appZg83GywzAAXvlu')
 base.all('Contacts')
@@ -9,15 +9,15 @@ base.all('Contacts')
 table = Table('keyTRu7XDdf9Ky5Q0', 'appZg83GywzAAXvlu', 'Contacts')
 table.all()
 
-//Belirli bir özelliğe göre arama ve tüm datasını alma
+#Belirli bir özelliğe göre arama ve tüm datasını alma
 from pyairtable.formulas import match
 formula = match({"Phone Number": "136-130-4036"})
 table.all(formula=formula)
 
-// ID'yi girerek veri güncelleme
+#ID'yi girerek veri güncelleme
 table.update('rec0PV4pfANoIjocD', {"Email": "a@gmail.com"})
 
-// Belirli bir özelliğe göre arayıp eşleşme sonucundaki diğer veriyi çekme
+#Belirli bir özelliğe göre arayıp eşleşme sonucundaki diğer veriyi çekme
 formula = match({"Phone Number": "136-130-4036"})
 table.all(formula = formula)
 print(table.first(fields = "Email" ))
